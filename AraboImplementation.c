ef5cb54731647ae7b96f03a46f157bc69fb8f44b
Arabo Hovsepian <ahovsepian@cpp.edu>
2022-03-05 22:54:32 -0800

    v 0.2

--- AraboImplementation.c
+++ AraboImplementation.c
@@ -1,27 +1,31 @@
-#include "AraboH_header.h"
+#include "AraboHeader.h"
 
-int totalNumDaysOnTrip ()
+int totalNumDaysOnTrip()
 {
+    int totalDays;
+
     do
     {
         printf("Please enter total number of days the person spent on their trip: ");
-        scanf("%d", totalDays);
+        scanf("%d", &totalDays);
 
         if (totalDays < 1)
         {
-            printf("Invalid input. Total number of days must be more than 0.")
+            printf("Invalid input. Total number of days must be more than 0.\n");
         }
     } while (totalDays < 1);
-    
+
     return totalDays;
 }
 
 float amountRoundTripAirfare()
 {
+    float dollarAirfare;
+
     do
     {
         printf("Please enter the amount of round-trip airfar: $");
-        scanf("%f", dollarAirfare);
+        scanf("%f", &dollarAirfare);
     } while (!dollarMileValidation(dollarAirfare));
 
     return dollarAirfare;
@@ -29,36 +33,46 @@ float amountRoundTripAirfare()
 
 float amountCarRentals()
 {
+    float dollarCarRental;
+
     do
     {
         printf("Please enter the amount of car rentals: $");
-        scanf("%f", dollarCarRental);
+        scanf("%f", &dollarCarRental);
     } while (!dollarMileValidation(dollarCarRental));
 
     return dollarCarRental;
 }
 
-void milesDriven()
+float milesDriven(float cDollarCarRental)
 {
-    int miles;
+    float miles;
 
     do
     {
-        if (dollarCarRental == 0)
-        {
-            printf("Please enter the amount of miles driven: ");
-            scanf("%d", miles);
-        }
+        printf("Please enter the amount of miles driven: ");
+        scanf("%d", &miles);
     } while (!dollarMileValidation(miles));
 
-    privateMileExpense = privateMileExpense * 0.27;
+    if (cDollarCarRental == 0)
+    {
+        return miles * 0.27;
+    }
+    else
+    {
+        return 0;
+    }
 }
 
 bool dollarMileValidation(float cDollarMile)
 {
     if (cDollarMile < 0)
     {
-        printf("Invalid input. Negative numbers are not allowed.");
+        printf("Invalid input. Negative numbers are not allowed.\n");
         return false;
+    }
+    else
+    {
+        return true;
     }
 }
